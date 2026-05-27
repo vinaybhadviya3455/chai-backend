39ab64f48d083bdce1e6ee40af786a8f5bd73e50
vinaybhadviya3455 <vinayvishvakarma3455@gmail.com>
2026-05-27 20:35:08 +0530

    refactor: update response format for current user retrieval and add TODO for avatar image utility

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -317,7 +317,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res) => {
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new Apiresponse (200,req.user,"Current user fetched successfully"))
 })
 
 
@@ -359,6 +359,9 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
     }
 
     //now upload on cloudinarry
+
+
+    //TODO: delete old image assignment make a new utility function
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
